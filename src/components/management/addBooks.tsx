89025d781069ff03cee72eeacf67067757bcac11
Mike <weldonconsulting@gmail.com>
2018-05-06 15:57:45 -0700

    add title input, common change handler

--- src/components/management/addBooks.tsx
+++ src/components/management/addBooks.tsx
@@ -1,24 +1,29 @@
 import * as React from 'react'
 import { RouteComponentWrapper } from '../index'
 import { Button, Grid, Row, Col, FormGroup, ControlLabel, FormControl, FormControlProps, HelpBlock } from 'react-bootstrap'
-class AddBooks extends React.Component<{}, { isbnValue: string }> {
+
+type AddBooksProps = {
+  isbn: string
+  title: string
+}
+
+class AddBooks extends React.Component<{}, AddBooksProps> {
 
   constructor(props: {}) {
     super(props);
-    this.state = { isbnValue: '' };
+    this.state = { isbn: '', title: '' };
   }
 
   private handleIsbnSearchClick = (e: React.FormEvent<HTMLInputElement>) => {
     e.currentTarget.value
   }
 
-  private handleIsbnUpdate = (e: React.FormEvent<FormControlProps>) => {
-
-    this.setState({ isbnValue: e.currentTarget.value as string });
+  private handleChangeFor = (propName: keyof AddBooksProps) => (e: React.FormEvent<FormControlProps>) => {
+    this.setState({ ...this.state, [propName]: e.currentTarget.value });
   }
 
   private validateIsbnValue() {
-    const length = this.state.isbnValue.length;
+    const length = this.state.isbn.length;
     if (length == 10 || length == 13) return 'success';
     if (length > 0) return 'error';
     return null;
@@ -34,8 +39,8 @@ class AddBooks extends React.Component<{}, { isbnValue: string }> {
         </Row>
         <Row className="display-flex">
           <Col md={9} xs={9}>
-            <FormGroup controlId="formBasicText" validationState={this.validateIsbnValue()}>
-              <FormControl type="text" value={this.state.isbnValue} placeholder="Enter isbn" onChange={this.handleIsbnUpdate} />
+            <FormGroup controlId="isbnInput" validationState={this.validateIsbnValue()}>
+              <FormControl type="text" value={this.state.isbn} placeholder="Enter isbn" onChange={this.handleChangeFor('isbn')} />
               {/* commented out till I know if I need it <FormControl.Feedback /> */}
               <HelpBlock>Enter a 10 digit or 13 digit isbn.</HelpBlock>
             </FormGroup>
@@ -43,8 +48,15 @@ class AddBooks extends React.Component<{}, { isbnValue: string }> {
           <Col md={3} xs={3}>
             <Button className="align-middle">Search</Button>
           </Col>
-          <Col md={12} xs={12}>
-            value: {this.state.isbnValue}
+        </Row>
+        <Row className="display-flex">
+          <Col md={1} xs={1}>
+            <ControlLabel>Title</ControlLabel>
+          </Col>
+          <Col md={11} xs={11}>
+            <FormGroup controlId="titleInput">
+              <FormControl type="text" value={this.state.title} placeholder="Enter title" onChange={this.handleChangeFor('title')} />
+            </FormGroup>
           </Col>
         </Row>
       </Grid>
