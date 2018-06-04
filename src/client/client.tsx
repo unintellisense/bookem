bd91d0935bd33d3d54de005195ae1cb161787695
Mike <weldonconsulting@gmail.com>
2018-06-03 20:29:05 -0700

    redux-toastr wip

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -3,6 +3,7 @@ import * as ReactDOM from 'react-dom';
 import { App } from './app';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 import './custom.css'
 
 ReactDOM.render(
--- src/client/state/index.ts
+++ src/client/state/index.ts
@@ -1,14 +1,17 @@
-import { combineReducers } from 'redux';
+import { combineReducers, Reducer } from 'redux';
 import { userReducer, UserState } from './user';
 import { ManageState, manageReducers } from './manage'
 export { ActionTypeKeys } from './actionTypes'
+import { reducer as toastrReducer, ToastrState } from 'react-redux-toastr';
 
 export interface AppState {
   user: UserState
   manage: ManageState
+  toastr: any
 };
 
 export const appReducers = combineReducers<AppState>({
   user: userReducer,
-  manage: manageReducers
+  manage: manageReducers,
+  toastr: toastrReducer
 });
