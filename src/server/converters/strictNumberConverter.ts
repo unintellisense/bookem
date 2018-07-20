2b99d798414a6dcae9efc9cc9ee22119aec53964
Mike <weldonconsulting@gmail.com>
2018-07-20 08:52:49 -0700

    fix strictNumberConverter

--- src/server/converters/strictNumberConverter.ts
+++ src/server/converters/strictNumberConverter.ts
@@ -4,9 +4,19 @@ import { BadRequest } from "ts-httpexceptions";
 @Converter(Number)
 export class StrictNumberConverter implements IConverter {
   deserialize<T>(data: string, target: any, baseType: T, deserializer: IDeserializer): String | Number | Boolean | void {
-    if (typeof data !== 'number')
-    throw new BadRequest(`'${data}' is not a valid number.`);
-    return data;
+    switch (typeof data) {
+      case 'number':
+        return data;
+
+      case 'string':
+        let numberVal = Number.parseInt(data);
+        if (isNaN(numberVal))
+          throw new BadRequest(`'${data}' is not a valid number.`);
+        return numberVal;
+
+      default:
+        throw new BadRequest(`'${data}' is not a valid number.`);
+    }
   }
 
   serialize(object: String | Number | Boolean): any {
