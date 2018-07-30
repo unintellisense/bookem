a9400950f7b4e4e6e2aafc24484bf72103a91a40
Mike <weldonconsulting@gmail.com>
2018-07-29 23:55:56 -0700

    ViewBookSearchOptionList wip

--- src/client/components/management/viewBook/viewBookSearchControl/viewBookSearchOptionList.tsx
+++ src/client/components/management/viewBook/viewBookSearchControl/viewBookSearchOptionList.tsx
@@ -1,8 +1,41 @@
 import * as React from 'react';
 import { Table, Button } from 'react-bootstrap'
+import { IBook } from '../../../../../shared/dto/ibook'
+
+const BookFieldNames: (keyof IBook)[] = [
+  "authors",
+  "bookSeriesNumber",
+  "categories",
+  "description",
+  "isbn",
+  "isFiction",
+  "libraryIdentifier",
+  "title",
+  "yearPublished"
+]
+
+const BookFieldDescriptiveNames: { [key in keyof IBook]: string } = {
+  title: 'Title', // str
+  authors: 'Authors', // str
+  bookSeriesNumber: 'Book Series Number', // num
+  categories: 'Categories', // str
+  description: 'Description', // str
+  isbn: 'ISBN', // str
+  isFiction: 'Fiction?', // bool
+  libraryIdentifier: 'Identifier', // str
+  yearPublished: 'Year Published' // num
+
+}
+
+class ViewBookSearchOption extends React.Component {
+
+}
+
+type ViewBookSearchOptionlistProps = {
+
+}
 
 export class ViewBookSearchOptionlist extends React.Component {
-
 
   render() {
     return <div>
@@ -10,21 +43,24 @@ export class ViewBookSearchOptionlist extends React.Component {
       <Table condensed bordered striped>
         <thead>
           <tr>
-            <th className={"col-xs-3"}>Field</th>
-            <th className={"col-xs-1"}>Operation</th>
+            <th className={"col-xs-4"}>Field</th>
             <th className={"col-xs-6"}>Value</th>
             <th className={"col-xs-2"}><Button block>Add</Button></th>
           </tr>
         </thead>
         <tbody>
-          {['one', 'two', 'three'].map(val => {
-            return <tr>
-              <td className={"col-xs-3"}>Title</td>
-              <td className={"col-xs-1"}>=</td>
-              <td className={"col-xs-6"}>{val}</td>
-              <td className={"col-xs-2"}><Button block>Remove</Button></td>
-            </tr>
-          })}
+          <tr>
+            <td className={"col-xs-4"}>
+              <select>
+                {
+                  BookFieldNames.map(name => {
+                    return <option>{BookFieldDescriptiveNames[name]}</option>
+                  })
+                }
+              </select></td>
+            <td className={"col-xs-6"}><text /></td>
+            <td className={"col-xs-2"}><Button block>Remove</Button></td>
+          </tr>
         </tbody>
       </Table>
     </div>
