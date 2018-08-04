ac413c368e34fc05957bd16953bb73f97be0484c
Mike <weldonconsulting@gmail.com>
2018-08-04 00:18:44 -0700

    mobile & desktop styling WIP

--- src/client/components/management/viewBook/viewBookSearchControl/viewBookSearchOptions/viewBookSearchOption.tsx
+++ src/client/components/management/viewBook/viewBookSearchControl/viewBookSearchOptions/viewBookSearchOption.tsx
@@ -15,23 +15,22 @@ export class ViewBookSearchOption extends React.Component<ViewBookSearchOptionPr
 
     return <ListGroupItem>
       <Row>
-        <Col>
-          <Col xs={4}>
-            <select style={{ width: "12rem" }} value={this.props.field} onChange={(e) => this.props.onChangeValue(e.target.value, this.props.idx)} >
-              {
-                this.props.allFields.map(field => {
-                  return <option value={field.shortName} key={field.shortName}>{field.descName}</option>
-                })
-              }
-            </select>
-          </Col>
-          <Col xs={6}>
-            <input type="text" />
-          </Col>
-          <Col xs={2}>
-            <Button block onClick={() => { this.props.onRemove(this.props.idx) }}>Remove</Button>
-          </Col>
+        <Col xs={4} md={4}>
+          <select style={{ width: "12rem" }} value={this.props.field} onChange={(e) => this.props.onChangeValue(e.target.value, this.props.idx)} >
+            {
+              this.props.allFields.map(field => {
+                return <option value={field.shortName} key={field.shortName}>{field.descName}</option>
+              })
+            }
+          </select>
         </Col>
+        <Col xs={6} xsOffset={2} md={2} mdOffset={0} mdPush={5}>
+          <Button block onClick={() => { this.props.onRemove(this.props.idx) }}>&#9587;</Button>
+        </Col>
+        <Col xs={10} md={6}>
+          <input type="text" />
+        </Col>
+
       </Row>
     </ListGroupItem>
   }
