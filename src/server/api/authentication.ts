e6cdb17885846bf88a90a7e1ef80749e902a4fd2
Mike <weldonconsulting@gmail.com>
2018-08-27 21:20:59 -0700

    clear cookies, continue to figure out session/state issue

--- src/server/api/authentication.ts
+++ src/server/api/authentication.ts
@@ -1,12 +1,12 @@
-import { Controller, Get, Post, Req, Status } from "@tsed/common";
+import { Controller, Get, Post, Req, Status, Res } from "@tsed/common";
 import { NotFound } from 'ts-httpexceptions';
 import { AuthState, LoginState } from '../../shared/dto/auth';
 
 @Controller("/auth")
-export class ManageController {
+export class AuthController {
 
   @Get("/state")
-  async getState(@Req() request: Express.Request, ): Promise<AuthState> {
+  async getState(@Req() request: Express.Request): Promise<AuthState> {
     if (request.user) {
       return { loginState: LoginState.LoggedIn, user: request.user };
     }
@@ -15,11 +15,13 @@ export class ManageController {
 
   @Post("/logout")
   @Status(204)
-  async logOut(@Req() request: Express.Request, ) {
+  async logOut(@Req() request: Express.Request, @Res() response: Express.Response) {
     if (request.user) {
       request.logout();
+      response.clearCookie();
       return null;
     }
+    response.clearCookie();
     throw new NotFound("no login session.");
   }
 
