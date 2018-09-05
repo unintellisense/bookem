186726aa68119b7bf58348ffa96bc74e01146719
Mike <weldonconsulting@gmail.com>
2018-09-05 01:43:15 -0700

    hrmnmm

--- src/server/util/validationUtil.ts
+++ src/server/util/validationUtil.ts
@@ -4,12 +4,12 @@ type ValidationArg<F> = F extends (arg: infer U) => any ? U : never
 
 type ValidationCall<F extends keyof Validator> = [F, ValidationArg<Validator[F]>]
 
-let call: ValidationCall<'isLength'> = ['isLength', { min: 1, max: 5 }]
+let call: ValidationCall<'isLength'> = ['isLength', { min: 1, max: '5x' }] // this breaks properly
 
-function bodyValidationByType<T>(typeProps: Partial<{ [prop in keyof T]: ValidationCall }>) {
+function bodyValidationByType<T>(typeProps: Partial<{ [prop in keyof T]: ValidationCall<keyof Validator> }>) {
 
 }
 
 body('').isLength({ min: 1, max: 5 })
 
-bodyValidationByType<{ x: string, y: string }>({ max: 5 })
+bodyValidationByType<{ x: string, y: string }>({ x: ['isLength', { shouldntwork: 1 }] }) // but not here???
