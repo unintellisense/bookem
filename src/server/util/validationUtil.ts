a7e077b83d139251a8f9f4a9d46a8bfdc383e4cc
Mike <weldonconsulting@gmail.com>
2018-09-05 01:54:43 -0700

    ...

--- src/server/util/validationUtil.ts
+++ src/server/util/validationUtil.ts
@@ -4,7 +4,9 @@ type ValidationArg<F> = F extends (arg: infer U) => any ? U : never
 
 type ValidationCall<F extends keyof Validator> = [F, ValidationArg<Validator[F]>]
 
-let call: ValidationCall<'isLength'> = ['isLength', { min: 1, max: '5x' }] // this breaks properly
+let call1: ValidationCall<'isLength'> = ['isLength', { min: 1, max: '5x' }] // this breaks properly
+
+let call2: ValidationCall<keyof Validator> = ['isLength', { min: 1, max: '5x' }] // this doesnt, cant infer arg?
 
 function bodyValidationByType<T>(typeProps: Partial<{ [prop in keyof T]: ValidationCall<keyof Validator> }>) {
 
