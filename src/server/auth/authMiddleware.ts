48f7d7ee6cb09b0bb54ea316b9336730096529b9
Mike <weldonconsulting@gmail.com>
2018-09-09 00:59:10 -0700

    drop old tsded stuff, and tsed packages

--- src/server/auth/authMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import * as Passport from 'passport';
-import { OAuth2Strategy as GoogleStrategy } from 'passport-google-oauth';
-import * as session from 'express-session';
-import { Service, BeforeRoutesInit, AfterRoutesInit, ServerSettingsService, Inject, ExpressApplication } from "@tsed/common";
-import { Request, Response } from 'express'
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, HOST } from './../config'
-import { UserInfo } from './../../shared/dto/auth'
-import { APP_COOKIE_NAME } from './../constants'
-
--- src/server/db/user.ts
+++ src/server/db/user.ts
@@ -1,33 +1,24 @@
 import { Model, JsonSchema } from 'objection';
-import { Required, Property, AllowTypes, Allow } from "@tsed/common";
 import { IUser } from '../../shared/dto/iuser';
 
 export default class User extends Model implements IUser {
-  @Required()
+  
   firstName: string;
 
-  @Required()
   lastName: string;
 
-  @Required()
   email: string;
 
-  @Required()
   externalIdentifier: string
 
-  @Required()
   externalProvider: string
 
-  @Required()
   creationDate: Date
 
-  @Required()
   lastLogin: Date
 
-  @Required()
   enabled: boolean
 
-  @Required()
   type: 'user' | 'admin'
 
   static get tableName() { return 'user'; }
