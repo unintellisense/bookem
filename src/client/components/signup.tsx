3f373189097e6692ef1c0b74919bfe390db786dc
Mike <weldonconsulting@gmail.com>
2018-09-20 00:19:21 -0700

    little bit more signup page work

--- src/client/components/signup.tsx
+++ src/client/components/signup.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react'
-import { Form, FormControlProps, Col, ControlLabel, FormControl } from 'react-bootstrap'
+import { Form, FormControlProps, Col, ControlLabel, FormControl, Alert, Button, Row } from 'react-bootstrap'
+import { style, media } from 'typestyle';
 import { InvisibleRouteItem } from '../route'
 import { IUser, UserSelfModifiable } from '../../shared/dto/iuser';
 
 type SignupState = { [prop in keyof UserSelfModifiable]: UserSelfModifiable[prop] }
 
+const DefaultState = {
+  firstName: '',
+  lastName: ''
+}
+
+const buttonMarginTopStyle = style({
+  marginTop: '27px'
+})
+
 export class Signup extends React.Component<{}, SignupState> {
 
   constructor(props) {
     super(props);
-    this.state = { firstName: '', lastName: '' };
+    this.state = DefaultState;
   }
 
   private handleChangeForUser = (propName: keyof IUser) => (e: React.FormEvent<FormControlProps>) => {
@@ -19,11 +29,17 @@ export class Signup extends React.Component<{}, SignupState> {
   }
 
   handleSubmit = (e: React.FormEvent<any>) => {
-    e.preventDefault
+    e.preventDefault();
+  }
+
+  handleReset = (e: React.FormEvent<any>) => {
+    e.preventDefault();
+    this.setState(DefaultState);
   }
 
   render() {
     return <Form horizontal className="container-fluid" onSubmit={this.handleSubmit}>
+      <Alert>Looks like you are a new user. Please use the form below to let us know some information about you.</Alert>
       <Col sm={12} >
         <ControlLabel>First Name</ControlLabel>
         <FormControl type="text" value={this.state.firstName || ''} placeholder="please enter first name" onChange={this.handleChangeForUser('firstName')} />
@@ -31,6 +47,12 @@ export class Signup extends React.Component<{}, SignupState> {
       <Col sm={12} >
         <ControlLabel>Last Name</ControlLabel>
         <FormControl type="text" value={this.state.lastName || ''} placeholder="please enter last name" onChange={this.handleChangeForUser('lastName')} />
+      </Col>
+      <Col sm={12} >
+        <Button block className={buttonMarginTopStyle} type="submit">Submit</Button>
+      </Col>
+      <Col sm={12} >
+        <Button block className={buttonMarginTopStyle} type="button" onClick={this.handleReset}>Reset</Button>
       </Col>
     </Form>
   }
