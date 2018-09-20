b4656fa6607813df7b4335c549cfd88a5e2e53ae
Mike <weldonconsulting@gmail.com>
2018-09-20 16:28:16 -0700

    login page header/alert to match signup page

--- src/client/components/login.tsx
+++ src/client/components/login.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { PageHeader } from 'react-bootstrap'
+import { Alert } from 'react-bootstrap'
 import { StandardRouteItem } from '../route'
 
 class Login extends React.Component {
@@ -7,7 +7,7 @@ class Login extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <PageHeader className="text-center">Login</PageHeader>
+        <Alert className="text-center">Please select a login method below.</Alert>
         <a href="/api/auth/google" className="btn btn-info" role="button" >Google</a>
       </div>
     )
